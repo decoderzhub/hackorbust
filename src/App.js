8854c64f7b8dd06549aa0e523c507218d71f03e6
decoderzhub <darin.j.manley@gmail.com>
2022-12-20 00:32:51 -0800

    Update Logout

--- src/App.js
+++ src/App.js
@@ -3,11 +3,14 @@ import "./App.css";
 import Login from "./Pages/Login";
 import Register from "./Pages/Register";
 import Home from "./Pages/Home";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import {
   BrowserRouter as Router,
   Routes,
   Route,
   useNavigate,
+  useLocation
 } from "react-router-dom";
 import { app } from "./Utilities/firebase-config";
 import {
@@ -20,46 +23,62 @@ function App() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [username, setUsername] = useState("");
-  const [state, setState] = useState('');
+  const [state, setState] = useState("");
   const navigate = useNavigate();
+  const location = useLocation();
 
   useEffect(() => {
+    if (location.setState === ""){
+      return
+    }
+
     let authToken = sessionStorage.getItem("Auth Token");
 
     if (authToken) {
       navigate("/home");
     }
-    console.log(state);
-    console.log(email, password);
+    //console.log(state);
+    //console.log(email, password);
     const authentication = getAuth();
     if (state === "Register") {
-      createUserWithEmailAndPassword(authentication, email, password).then(
-        (response) => {
+      createUserWithEmailAndPassword(authentication, email, password)
+        .then((response) => {
           navigate("/home");
           sessionStorage.setItem(
             "Auth Token",
             response._tokenResponse.refreshToken
           );
-        }
-      );
+        })
+        .catch((error) => {
+          toast.error("Email Already in Use");
+          if (error.code === "auth/email-already-in-use") {
+          }
+        });
     }
     if (state === "Login") {
-      signInWithEmailAndPassword(authentication, email, password).then(
-        (response) => {
-          navigate("/home");
+      signInWithEmailAndPassword(authentication, email, password)
+        .then((response) => {
+          navigate("/home", {setEmail, setPassword});
           sessionStorage.setItem(
             "Auth Token",
             response._tokenResponse.refreshToken
           );
-        }
-      );
+        })
+        .catch((error) => {
+          if (error.code === "auth/wrong-password") {
+            toast.error("Please check the Password");
+          }
+          if (error.code === "auth/user-not-found") {
+            toast.error("Please check the Email");
+          }
+        });
     }
-
-  }, [state]);
+  }, [state, email, password]);
 
   return (
     <div className="App">
       <>
+        <ToastContainer />
         <Routes>
           <Route
             path="/login"
