94a6bfd8353cec694da7337dec1955833f584434
decoderzhub <darin.j.manley@gmail.com>
2022-12-22 03:52:41 -0800

    update appbar

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -1,5 +1,4 @@
 import { React, useEffect, useState } from "react";
-import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
@@ -7,16 +6,16 @@ import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import CssBaseline from "@mui/material/CssBaseline";
 import Grid from "@mui/material/Grid";
+import Stack from "@mui/material/Stack";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
 import Link from "@mui/material/Link";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { useNavigate } from "react-router-dom";
+import logo from "../Images/Logo.png";
 import "../CSS/DarkSideMoon.css";
 import ShootingStars from "../Components/ShootingStars";
 import ResponsiveAppBar from "../Components/ResponsiveAppBar";
-import Stack from "@mui/material/Stack";
-
 
 function Copyright() {
   return (
@@ -28,6 +27,22 @@ function Copyright() {
       {new Date().getFullYear()}
       {"."}
     </Typography>
+  );
+}
+
+function Logo() {
+  return (
+    <img
+      src={logo}
+      alt="Logo"
+      style={{
+        height: "60%",
+        width: "100%",
+        marginTop: "-100px",
+        marginBottom: "0px",
+        padding: "0px 0px 0px 0px",
+      }}
+    />
   );
 }
 
@@ -79,7 +94,7 @@ export default function Home() {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <ResponsiveAppBar handleLogout={handleLogout} />
+      <ResponsiveAppBar handleLogout={handleLogout}/>
       <main>
         <div className="background-container">
           <div className="stars" />
@@ -91,13 +106,16 @@ export default function Home() {
             alt="The Moon"
           />
           <ShootingStars />
+          <Container maxWidth="sm" style={{ zIndex: "1000 !important" }}>
+            <Logo />
+          </Container>
           <Container maxWidth="sm">
             <Typography
               variant="h6"
               align="center"
               color="text.primary"
               paragraph
-              style={{ marginTop: "100px" }}
+              style={{ marginTop: "-100px" }}
             >
               Weclome to HackOrBust where you can learn how to penetrate
               wireless networks. These wireless networks range from routers to
@@ -111,16 +129,18 @@ export default function Home() {
               justifyContent="center"
               style={{ paddingBottom: "30px" }}
             >
+              <Button variant="contained">Subscribe</Button>
+              <Button variant="outlined">Learn More</Button>
             </Stack>
           </Container>
-          <Container sx={{ py: 1, paddingTop: "0" }} maxWidth="md">
-            <Grid container spacing={12}>
+          <Container sx={{ py: 1 }} maxWidth="md">
+            <Grid container spacing={8}>
               {cards.map((card, index) => (
                 <Grid item key={card} xs={12} sm={6} md={4}>
                   <Card
                     sx={{
                       height: "100%",
-                      width: "100%",
+                      width: "120%",
                       display: "flex",
                       flexDirection: "column",
                     }}
@@ -162,27 +182,22 @@ export default function Home() {
               ))}
             </Grid>
           </Container>
+
+          {/* Footer */}
+          <Typography variant="h6" align="center" gutterBottom>
+            Don't give up...
+          </Typography>
+          <Typography
+            variant="subtitle1"
+            align="center"
+            color="text.secondary"
+            component="p"
+          >
+            You&apos;re A God D*#? Wi-Fi Professional!
+          </Typography>
+          <Copyright />
+          {/* End footer */}
         </div>
-        <Box
-          component="footer"
-          sx={{
-            py: 3,
-            px: 2,
-            mt: "auto",
-            backgroundColor: (theme) =>
-              theme.palette.grey === "dark"
-                ? theme.palette.grey[900]
-                : theme.palette.grey[900],
-            marginTop:40
-          }}
-        >
-          <Container maxWidth="sm">
-            <Typography variant="body1">
-              You&apos;re A God D*#? Wi-Fi Professional!
-            </Typography>
-            <Copyright />
-          </Container>
-        </Box>
       </main>
     </ThemeProvider>
   );
