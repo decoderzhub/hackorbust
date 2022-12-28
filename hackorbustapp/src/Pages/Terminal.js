cf9396b4705861711771407f0d231c65a6f9a10e
decoderzhub <darin.j.manley@gmail.com>
2022-12-27 17:20:53 -0800

    update terminal

--- hackorbustapp/src/Pages/Terminal.js
+++ hackorbustapp/src/Pages/Terminal.js
@@ -1,27 +1,25 @@
 import { React, useEffect, useState } from "react";
-import Button from "@mui/material/Button";
-import Card from "@mui/material/Card";
-import CardActions from "@mui/material/CardActions";
-import CardContent from "@mui/material/CardContent";
-import CardMedia from "@mui/material/CardMedia";
 import CssBaseline from "@mui/material/CssBaseline";
 import Grid from "@mui/material/Grid";
-import Typography from "@mui/material/Typography";
-import Container from "@mui/material/Container";
+import Box from "@mui/system/Box";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import "../CSS/DarkSideMoon.css";
-import Stack from "@mui/material/Stack";
-import { useLocation, useSearchParams } from "react-router-dom";
+import { useSearchParams } from "react-router-dom";
+import VerticalLinearStepper from "../Components/VerticalLinearStepper";
+import ShootingStars from "../Components/ShootingStars";
+import { Typography } from "@mui/material";
+
 
 export default function Terminal(props) {
-    const [searchparams] = useSearchParams();
-    const [course, setCourse] = useState("");
-    
+  const [searchparams] = useSearchParams();
+  const [course, setCourse] = useState("");
 
   useEffect(() => {
     let temp = searchparams.get("course");
     console.log(temp);
-    setCourse(temp);
+    if (temp) {
+      setCourse(temp);
+    }
 
     let authToken = sessionStorage.getItem("Auth Token");
 
@@ -37,8 +35,17 @@ export default function Terminal(props) {
     };
   }, []);
 
-  function handleClick() {
-    props.navigate("/novice");
+  function Header() {
+    switch (course) {
+        case "airmon-ng":
+            return ( 
+                <Typography variant="h5" component="h2" marginTop="2%">
+                learn {course} command instance
+                </Typography>
+            )
+        default:
+            break;
+    }
   }
 
   const theme = createTheme({
@@ -60,8 +67,48 @@ export default function Terminal(props) {
             src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/1231630/moon2.png"
             alt="The Moon"
           />
-
-          <div>{course}</div>
+          <ShootingStars />
+          <Box sx={{ flexGrow: 1 }}>
+                <Header/>
+            <Grid container spacing={2}>
+              <Grid xs={8}>
+                <Box sx={{ flexGrow: 1 }}>
+                  <iframe
+                    title="terminal"
+                    src={
+                      "http://10.0.0.91:6807?hostname=kali&username=kali&password=a2FsaQo="
+                    }
+                    style={{
+                      position: "relative",
+                      marginTop: 50,
+                      width: "95%",
+                      height: "100vh",
+                      opacity: 0.7,
+                      outlineStyle: "outset",
+                      outlineColor: "red",
+                    }}
+                  ></iframe>
+                </Box>
+              </Grid>
+              <Grid xs={4}>
+                <Box
+                  sx={{
+                    flexGrow: 1,
+                    backgroundColor: "rgb(18, 18, 18, 0.7)",
+                    height: "100vh",
+                    alignContent: "center",
+                    outlineStyle: "solid",
+                    outlineColor: "red",
+                    marginRight: "3%"
+                  }}
+                >
+                  <div style={{ marginTop: 50, marginLeft: "10%" }}>
+                    <VerticalLinearStepper />
+                  </div>
+                </Box>
+              </Grid>
+            </Grid>
+          </Box>
         </div>
       </main>
     </ThemeProvider>
