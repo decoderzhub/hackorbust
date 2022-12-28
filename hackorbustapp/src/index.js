3fda739daf98ad41df4362c6bec93b7366354702
decoderzhub <darin.j.manley@gmail.com>
2022-12-27 19:27:54 -0800

    process env

--- hackorbustapp/src/index.js
+++ hackorbustapp/src/index.js
@@ -5,6 +5,7 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter as Router } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById("root"));
+process.env.CI = false
 root.render(
   <React.StrictMode>
     <Router>
