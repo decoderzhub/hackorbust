ed64fc877d30e5cc9fc42acf1a33c6b50b888278
decoderzhub <darin.j.manley@gmail.com>
2023-01-03 00:23:24 -0800

    update url

--- hackorbustapp/src/Pages/Terminal.js
+++ hackorbustapp/src/Pages/Terminal.js
@@ -85,7 +85,7 @@ export default function Terminal(props) {
                   <iframe
                     title="terminal"
                     src={
-                      "https://hackorbust.ddns.net:4433/?port=6808&hostname=kali&username=wifipro&password=a2FsaQo=&name=12345"
+                      "https://hackorbust.ddns.net:4433/?port=6808&hostname=kali&username=wifipro&password=a2FsaQo=&command=tmux"
                     }
                     style={{
                       position: "relative",
